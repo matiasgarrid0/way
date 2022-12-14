0b0fbbadebb997ede2a9e98823510fc1147cbca3
Matias Garrido <matias17garrido@gmail.com>
2022-12-14 19:55:36 -0300

    navbar

--- way/pages/index.js
+++ way/pages/index.js
@@ -1,11 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Navbar from "../components/Navbar"
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-     esto es de mati
+    <div>
+      <Navbar/>
+     
     </div>
   )
 }
