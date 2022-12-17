b942ad7b63becde456aa6ee44bc6e22beea326b1
Matias Garrido <matias17garrido@gmail.com>
2022-12-17 13:03:40 -0300

    cursos

--- way/pages/cursos.js
+++ way/pages/cursos.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Navbar from '../components/Navbar'
+import { TextField, Box, Button } from '@mui/material'
 
 const cursos = () => {
   return (
     <div>
       <Navbar/>
+      <h1>Cursos</h1>
+        <Button type='contained'>Crear Curso</Button>
     </div>
   )
 }
