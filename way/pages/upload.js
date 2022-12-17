6e5252413fec61bdaebe54ad116d56b72992b5a3
lean4339 <lean4339@gmail.com>
2022-12-17 15:27:49 -0300

    cambios de foto

--- way/pages/upload.js
+++ way/pages/upload.js
@@ -4,15 +4,26 @@ import { TextField } from '@mui/material'
 export default function Upload  (props) {
   
   const onChange = async (e) => {
-    const response = await axios.post('/api/upload/1');
+    const formData = new FormData();
+    formData.append('file',e.target.files[0]);
+    const config = {
+      headers: { 'content-type': 'multipart/form-data' },
+      /*onUploadProgress: (event) => {
+        console.log(`Current progress:`, Math.round((event.loaded * 100) / event.total));
+      },*/
+    };
+
+    const response = await axios.post('/api/upload/1', formData);
+
     console.log('response', response.data);
   };
 
   return (
     <>
     <input
-    onChange={onChange}
     type='file'
+    name='file'
+    onChange={onChange}
     />
     </>
     );
