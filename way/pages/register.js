afa137306e1a5a76454ba5c8c7aa8968e3babe5e
Matias Garrido <matias17garrido@gmail.com>
2023-01-05 12:07:14 -0300

    register

--- way/pages/register.js
+++ way/pages/register.js
@@ -1,9 +1,39 @@
-import { useState } from "react";
-import axios from "axios";
+import React, { useState } from "react";
+import { Button, TextField } from "@mui/material";
+import Navbar from "../components/Navbar";
+import { Box } from "@mui/system";
 
-export default function Register() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+const style = {
+  position: "absolute",
+  marginTop:"350px",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "#fff",
+  border: "1px solid #eaeaea",
+  boxShadow: 24,
+  p: 4,
+  maxHeight: 600,
+};
+const styleLogin = {
+  position: "absolute",
+  marginTop:"200px",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "#fff",
+  border: "1px solid #eaeaea",
+  boxShadow: 24,
+  p: 4,
+  maxHeight: 600,
+}
+
+const register = () => {
+  const [register, setRegister] = useState(false);
+
+  const handleRegister = () => {
+    setRegister(!register);
+  };
   const onSubmit = () => {
     axios
       .post("/api/register", { username, password })
@@ -12,19 +42,90 @@ export default function Register() {
   };
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={(e) => setUsername(e.target.value)}
-          type="text"
-          name="username"
-        />
-        <input
-          onChange={(e) => setPassword(e.target.value)}
-          type="password"
-          name="password"
-        />
-        <button type="submit">registrarse</button>
-      </form>
+      <Navbar />
+      <div>
+        {register ? (
+          <Box sx={styleLogin}>
+              <p style={{ textAlign: "center", fontSize: "25px", margin: "0" }}>
+              Ingresar
+            </p>
+            <TextField
+              label="Usuario"
+              name="name"
+              style={{ width: "100%", margin: "5px" }}
+            />
+            <TextField
+              type={"password"}
+              label="Contrase"
+              name="name"
+              style={{ width: "100%", margin: "5px" }}
+            />
+            <Button
+              color="success"
+              style={{ width: "100%", marginTop: "20px" }}
+              variant="contained"
+              type="sumbit"
+            >
+              Iniciar Sesión
+            </Button>
+          </Box>
+        ) : (
+          <Box sx={style}>
+            <p style={{ textAlign: "center", fontSize: "25px", margin: "0" }}>
+              Crear Cuenta
+            </p>
+            <TextField
+              label="Nombre"
+              name="name"
+              style={{ width: "100%", margin: "5px" }}
+            />
+            <TextField
+              label="Apellido"
+              name="name"
+              style={{ width: "100%", margin: "5px" }}
+            />
+            <TextField
+              label="Email"
+              name="name"
+              style={{ width: "100%", margin: "5px" }}
+            />
+            <TextField
+              label="Usuario"
+              name="name"
+              style={{ width: "100%", margin: "5px" }}
+            />
+            <TextField
+              type={"password"}
+              label="Contrase"
+              name="name"
+              style={{ width: "100%", margin: "5px" }}
+            />
+            <TextField
+              type={"password"}
+              label="Repetir Contrase"
+              name="name"
+              style={{ width: "100%", margin: "5px" }}
+            />
+            <Button
+              color="success"
+              style={{ width: "100%", marginTop: "20px" }}
+              variant="contained"
+              type="sumbit"
+            >
+              Registrarse
+            </Button>
+            <p style={{ width: "100%" }}>
+              Ya tienes cuenta?{" "}
+              <span>
+                <Button onClick={handleRegister}>Iniciar Sesión</Button>
+              </span>
+            </p>
+          </Box>
+        )}
+        <div></div>
+      </div>
     </div>
   );
-}
+};
+
+export default register;
